9b44054fac7dda190a9e08baf39463b909ca6642
James Ponder <james@squish.net>
2020-10-02 23:44:21 +0100

    fix

--- SetGames.bat
+++ SetGames.bat
@@ -1,1 +1,1 @@
-@powershell.exe -NoProfile -ExecutionPolicy Bypass -File Win10.ps1 -include Games.preset
+@powershell.exe -NoProfile -ExecutionPolicy Bypass -File Win10.ps1 -include Win10.psm1 -preset Games.preset
